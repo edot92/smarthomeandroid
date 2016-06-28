38f5eb7c7384b2712c2d0e30d9e3696c998a03cd
laptop lenovo <edot92@gmail.com>
2016-06-29 05:29:22 +0700

    first update 29_6_16 3_38kd

--- mikro/smarthome_29_juni_2016/HttpRequest_082210160003.ino
+++ mikro/smarthome_29_juni_2016/HttpRequest_082210160003.ino
@@ -16,67 +16,69 @@ void ambilStatusTombolWeb() {
     client.println("User-Agent: arduino-ethernet");
     client.println("Connection: close");
     client.println();
+
+    memset(dataWeb, 0, 100);
+    ambilDataWeb = false;
+    unsigned long  a = 0;
+    bool okekkkkkk = false;
+    cntDataWeb = 0;
+    while ( okekkkkkk == false && a < intervalAmbilweb) { //>5000
+      a++;
+      delay(1);
+      while (client.available() > 0) {
+
+        char c = client.read();
+
+        if (c == '(' && ambilDataWeb == false) {
+          ambilDataWeb = true;
+        };
+        if (ambilDataWeb == true && okekkkkkk == false) {
+          if ( c != '(' && c != ')') {
+
+            dataWeb[cntDataWeb] = c;
+            cntDataWeb++;
+          } else if ( c == ')') {
+            okekkkkkk = true;
+          }
+        }
+      }
+    }
+    if (ambilDataWeb == true) {
+      ambilDataWeb = false;
+      cntDataWeb = 0;
+      Serial.println();
+      Serial.println(dataWeb);
+      memset(dataWeb, 0, 100);
+
+    }
   }
   else {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
   }
 
-  memset(dataWeb, 0, 100);
-  ambilDataWeb = false;
-  unsigned long  a = 0;
-  bool okekkkkkk = false;
-  cntDataWeb = 0;
-  while ( okekkkkkk == false && a < intervalAmbilweb) { //>5000
-    a++;
-    delay(1);
-    while (client.available() > 0) {
 
-      char c = client.read();
-
-      if (c == '(' && ambilDataWeb == false) {
-        ambilDataWeb = true;
-      };
-      if (ambilDataWeb == true && okekkkkkk == false) {
-        if ( c != '(' && c != ')') {
-         
-          dataWeb[cntDataWeb] = c;
-          cntDataWeb++;
-        } else if ( c == ')') {
-          okekkkkkk = true;
-        }
-      }
-    }
-  }
-  if (ambilDataWeb == true) {
-    ambilDataWeb = false;
-    cntDataWeb = 0;
-    Serial.println();
-    Serial.println(dataWeb);
-    memset(dataWeb, 0, 100);
-
-  }
   client.flush();
   client.stop();
 
 }
 float random_;
-void kirimSuhuWeb() 
+void kirimSuhuWeb()
 {
-  char get[100]="GET /kirimSuhuWeb?suhu=";
-  char http[10]=" HTTP/1.1";
+  char get[100] = "GET /kirimSuhuWeb?suhu=";
+  char http[10] = " HTTP/1.1";
   char strSend[80];
   char suhu[6];
 
 
- random_= random(30.1,50.4);
-//4 is mininum width, 3 is precision; float value is copied onto buff
-dtostrf(random_, 4, 3, suhu);
-  strcat (get,suhu);
-   strcat (get,http);
-  
+  random_ = random(30.1, 50.4);
+  //4 is mininum width, 3 is precision; float value is copied onto buff
+  dtostrf(random_, 4, 3, suhu);
+  strcat (get, suhu);
+  strcat (get, http);
 
- if (client.connect(server, 80)) {
+
+  if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
     client.println(get);
@@ -84,45 +86,46 @@ dtostrf(random_, 4, 3, suhu);
     client.println("User-Agent: arduino-ethernet");
     client.println("Connection: close");
     client.println();
+    memset(dataWeb, 0, 100);
+    ambilDataWeb = false;
+    unsigned long  a = 0;
+    bool okekkkkkk = false;
+    cntDataWeb = 0;
+    while ( okekkkkkk == false && a < intervalAmbilweb) { //>5000
+      a++;
+      delay(1);
+      while (client.available() > 0) {
+
+        char c = client.read();
+
+        if (c == '(' && ambilDataWeb == false) {
+          ambilDataWeb = true;
+        };
+        if (ambilDataWeb == true && okekkkkkk == false) {
+          if ( c != '(' && c != ')') {
+
+            dataWeb[cntDataWeb] = c;
+            cntDataWeb++;
+          } else if ( c == ')') {
+            okekkkkkk = true;
+          }
+        }
+      }
+    }
+    if (ambilDataWeb == true) {
+      ambilDataWeb = false;
+      cntDataWeb = 0;
+      Serial.println();
+      Serial.println(dataWeb);
+      memset(dataWeb, 0, 100);
+
+    }
   }
   else {
     // kf you didn't get a connection to the server:
     Serial.println("connection failed");
   }
-  memset(dataWeb, 0, 100);
-  ambilDataWeb = false;
-  unsigned long  a = 0;
-  bool okekkkkkk = false;
-  cntDataWeb = 0;
-  while ( okekkkkkk == false && a < intervalAmbilweb) { //>5000
-    a++;
-    delay(1);
-    while (client.available() > 0) {
 
-      char c = client.read();
-
-      if (c == '(' && ambilDataWeb == false) {
-        ambilDataWeb = true;
-      };
-      if (ambilDataWeb == true && okekkkkkk == false) {
-        if ( c != '(' && c != ')') {
-         
-          dataWeb[cntDataWeb] = c;
-          cntDataWeb++;
-        } else if ( c == ')') {
-          okekkkkkk = true;
-        }
-      }
-    }
-  }
-  if (ambilDataWeb == true) {
-    ambilDataWeb = false;
-    cntDataWeb = 0;
-    Serial.println();
-    Serial.println(dataWeb);
-    memset(dataWeb, 0, 100);
-
-  }
   client.flush();
   client.stop();
 }
